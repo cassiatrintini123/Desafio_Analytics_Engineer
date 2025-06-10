5483f259bf4fc3f43319e29231ba2063df7c6bb5
Cássia Trintini <cassia.trintini@indicium.tech>
2025-06-10 20:51:59 +0000

    int_reasons adjustments

--- models/intermediate/int_reasons.sql
+++ models/intermediate/int_reasons.sql
@@ -1,25 +1,28 @@
 with 
     SALESREASON as (
         select *
-        from {{ ref('stg_erp__SALESREASON')}}
+        from {{ ref('stg_erp__SALESREASON') }}
     )
 
     , SALESORDERHEADERSALESREASON as (
         select *
-        from {{ ref('stg_erp__SALESORDERHEADERSALESREASON')}}
+        from {{ ref('stg_erp__SALESORDERHEADERSALESREASON') }}
     )
 
     , joined as (
         select 
             SALESORDERHEADERSALESREASON.PK_SALESORDERID
-            , SALESORDERHEADERSALESREASON.FK_SALESREASONID
-            , SALESREASON.PK_SALESREASONID
             , SALESREASON.SALESREASON_NAME
             , SALESREASON.REASONTYPE_NAME
         from SALESORDERHEADERSALESREASON
         left join SALESREASON on SALESORDERHEADERSALESREASON.FK_SALESREASONID = SALESREASON.PK_SALESREASONID
+        where SALESREASON.PK_SALESREASONID between 1 and 6
     )
 
-select * 
+select 
+    PK_SALESORDERID
+    , LISTAGG(distinct SALESREASON_NAME, ', ') within group (order by SALESREASON_NAME) as SALESREASON_NAMES
+    , LISTAGG(distinct REASONTYPE_NAME, ', ') within group (order by REASONTYPE_NAME) as REASONTYPE_NAMES
 from joined
+group by PK_SALESORDERID
 
